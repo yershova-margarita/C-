06e0c0294943581aecb1ea40ab8bf74225448933
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-23 19:56:40 +0300

    add random

--- Lsn1-example 3 Sum/Program.cs
+++ Lsn1-example 3 Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine("Первое число:" + numberA);
+int numberB = new Random().Next(1,10); 
+Console.WriteLine("Второе число:" + numberB);
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine("Сумма:" + result);
