340bff7473686a3435d6bb53229d06569d714b38
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-23 20:06:35 +0300

    Add Example 5 - IfElse

---
+++ Lsn5-example 5 IfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша") {
+    Console.WriteLine("Ура, это же Маша!");
+}
+else {
+    Console.WriteLine("Привет, " + userName);
+}
