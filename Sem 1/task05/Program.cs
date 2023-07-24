7df567a9fde56facc3d6a44a95dbbb7582d96357
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-24 15:34:07 +0300

    Добавлено решение через for

---
+++ Sem 1/task05/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая на вход принимает 
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N
+Console.WriteLine("Введите натуральное число");
+int userNum = Convert.ToInt32(Console.ReadLine());
+if (userNum > 0)
+{
+    for (int i = -userNum; i <= userNum; i++)
+    {
+        Console.Write($"{i}, ");
+    }
+}
+else
+{
+    Console.Write($"Число {userNum} не является натуральным");
+}
