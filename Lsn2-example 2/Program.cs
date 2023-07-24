e180147e9dca6ebf25a32fc9fb65ffc950837cce
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-24 10:11:19 +0300

    Add exampe2 - array

---
+++ Lsn2-example 2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] myArray = { 11, 23, 56, 34, 65, 67, 7 ,78, 39 };
+
+int max = Max(
+    Max(myArray[0], myArray[1], myArray[2]),
+    Max(myArray[3], myArray[4], myArray[5]),
+    Max(myArray[6], myArray[7], myArray[8]));
+Console.Write("Наибольшее число: " + max);
