92c2d004da2fdce7a9220a5696605e090ccc84cf
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-24 10:20:32 +0300

    Add example3 - find index elem

---
+++ Lsn2-example 3/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] exzArray = { 178, 200, 56, 94, 82, 467, 387, 677, 89 };
+int count = exzArray.Length;
+int find = 82;
+int i = 0;
+while (i < count)
+{
+    if(exzArray[i] == find)
+    {
+        Console.Write(i);
+        break;
+    }
+    i++;
+}
