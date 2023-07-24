332f0ea623e0b1a6cacc511e213523365093e258
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-24 15:55:55 +0300

    Добавлено нахождение первой и второй цифры трехзначного числа

--- Sem 1/task07/Program.cs
+++ Sem 1/task07/Program.cs
@@ -1,14 +1,23 @@
 ﻿// напишите программу, которая принимает на вход
-// трехзначное число и на ыходе показывает
+// трехзначное число и на выходе показывает
 // последнюю цифру этого числа
 Console.WriteLine("Введите трехзначное целое число");
 int userNum = Convert.ToInt32(Console.ReadLine());
 if ((userNum >= 100 && userNum <= 999) || (userNum >= -999 && userNum <= -100))
 {
-    int result = userNum % 10;
-    Console.WriteLine($"Последняя цифра числа {userNum} - {result}");
+    // последнюю цифру этого числа
+    int resultLast = userNum % 10;
+    // первую цифру этого числа
+    int resultFirst = userNum / 100;
+    // вторую цифру этого числа
+    int resultSecond = userNum / 10 % 10;
+    Console.WriteLine($"Первая цифра числа {userNum} - {resultFirst}");
+    Console.WriteLine($"Вторая цифра числа {userNum} - {resultSecond}");
+    Console.WriteLine($"Последняя цифра числа {userNum} - {resultLast}");
 }
 else
 {
     Console.WriteLine("Число не является трехзначным");
 }
+
+// первую цифру этого числа
