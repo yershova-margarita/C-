b966efbff96272be834c6b4d679a0435305a29be
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-24 15:48:10 +0300

    Нахождение последней цифры трехзначного числа

---
+++ Sem 1/task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает на вход
+// трехзначное число и на ыходе показывает
+// последнюю цифру этого числа
+Console.WriteLine("Введите трехзначное целое число");
+int userNum = Convert.ToInt32(Console.ReadLine());
+if ((userNum >= 100 && userNum <= 999) || (userNum >= -999 && userNum <= -100))
+{
+    int result = userNum % 10;
+    Console.WriteLine($"Последняя цифра числа {userNum} - {result}");
+}
+else
+{
+    Console.WriteLine("Число не является трехзначным");
+}
