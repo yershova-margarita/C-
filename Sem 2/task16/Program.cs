38dd4903a14e40d1fd811aa28b2b151586bc9277
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-28 18:58:21 +0300

    Добавлено решение задачи 16. Проверка квадрата двух чисел

---
+++ Sem 2/task16/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+bool Sqrt(int firstNum, int secondNum)
+{
+    int firstNumSqrt = firstNum * firstNum;
+    int secondNumSqrt = secondNum * secondNum;
+    return (firstNumSqrt == secondNum || secondNumSqrt == firstNum);
+};
+
+Console.WriteLine("Введите первое число: ");
+int userFirstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int userSecondNum = Convert.ToInt32(Console.ReadLine());
+
+bool userResult = Sqrt(userFirstNum, userSecondNum);
+Console.WriteLine(userResult ? "да" : "нет");
