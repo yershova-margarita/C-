22ed63b2d6d27520cbc83183116ff25ef6642d97
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-01 14:54:14 +0300

    Доавлено решение задачи 22 на вывод таблицы кваратов введенного числа

---
+++ Sem 3/task22/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.
+*/
+
+void SqrtTable(int n) 
+{
+    for (int i = 1; i <= n; i++) 
+    {
+        double sqrt = Math.Pow(i,2);
+        Console.WriteLine($"{i}  {sqrt}");
+    }
+}
+
+Console.WriteLine("Введите науральное число : ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum <= 0)
+{
+    Console.WriteLine("Вы ввели не натуральное число, повторите ввод");
+}
+SqrtTable(userNum);
