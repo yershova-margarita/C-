250783bf0357d5b421644514f0220292c7d20a38
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-02 13:26:29 +0300

    Добавлено решение задачи 28 на нахождение факториала (произведение чисел от 1 до n)

---
+++ Sem 4/task28/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int FactorialOfNumbers(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked        // работа с исключениями, чтобы не выйти за разрядность сетки
+        {
+            fact *= i; // fact = fact * i;
+        }
+    }
+    return fact;
+}
+
+Console.WriteLine("Введите натуральное число А :");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum > 0)
+{
+    Console.WriteLine($"Произведение чисел от 1 до {userNum} : {FactorialOfNumbers(userNum)}");
+}
+else
+{
+    Console.WriteLine($"Введенное число {userNum} не является натуральным, повторите ввод");
+}
