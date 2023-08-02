513741453461679e2dce273514d0a316c91fc20e
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-02 12:39:49 +0300

    Добавлено решение задачи 24 на нахождение суммы чисел от 1 до n. Обраотка исключений - checked

---
+++ Sem 4/task24/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked        // работа с исключениями, чтобы не выйти за разрядность сетки
+        {
+            sum += i; //sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите натуральное число А :");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum > 0)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {userNum} : {SumNumbers(userNum)}");
+}
+else
+{
+    Console.WriteLine($"Введенное число {userNum} не является натуральным, повторите ввод");
+}
