2783c63fd04cebf69d7dbc5d433559e8fad8c64e
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-02 14:58:02 +0300

    Добавлено решение задачи 30 на заполнение массива 0 и 1

---
+++ Sem 4/task30/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая выводит
+массив из 8 элементов, заполненный нулями и
+единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] myArray = new int[8];     // массив c 0
+FillArray(myArray);
