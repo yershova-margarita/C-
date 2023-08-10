dab453ec074c94abafe5f2b59848042f50bee58b
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-10 08:29:50 +0300

    Добавлено решение задачи 33 на поиск введенного числа в массиве

---
+++ Sem 5/task33/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+3; массив [6, 7, 19, 345, 3] -> да
+*/
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+bool SearchElemInArray(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int[] array = CreateArrayRndInt(12, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Введите число");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SearchElemInArray(array, userNum) ? "да" : "нет");
