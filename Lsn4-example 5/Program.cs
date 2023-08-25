f0673d033a298d47ebc6e6895622b96cb38cadf6
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-25 17:41:09 +0300

    Задача на числа Фибоначи

---
+++ Lsn4-example 5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Фибоначи
+
+double Fibonachi (int n)
+{
+    if(n==1 || n==2) return 1;
+    else return Fibonachi(n-1) + Fibonachi(n-2);
+}
+
+Console.WriteLine("Введите число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < userNum; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonachi(i)}");
+}
