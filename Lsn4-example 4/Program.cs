be53612b3b5d745119e60b6e75e2da7f8c70e10a
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-25 16:25:43 +0300

    Задача на рекурсию

---
+++ Lsn4-example 4/Program.cs
@@ -0,0 +1,13 @@
+﻿double Factorial(int n)
+{
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine("Введите число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < userNum; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
