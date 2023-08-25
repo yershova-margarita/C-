a0af26153a9d620c4fa51b44ead5b487082160e9
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-25 13:13:18 +0300

    Лекция 3

---
+++ Lsn3-example 1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст ...");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Текст сообщения", count: 4); // Именованные аргументы: по порядку писать необязательно.
+// Method21("Текст сообщения", 4);          // Неименованные аргументы - обязательно по порядку
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4 
+string Method4(int count, char text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, 'z');
+Console.WriteLine(res);
+
+string Method41(int count, char text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res1 = Method41(10, 'z');
+Console.WriteLine(res1);
---
+++ Lsn3-example 3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Дан текст. В тексте нужно все побелы заменить черточками,
+// маленькие буквы "к" заменить большими "К", а большие "С"
+// заменить маленькими "с"
+
+// string s = "qwerty"
+//             012345
+// s[3]     // r - посимвольное обращение
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+         if (text[i] == oldValue) result = result+ $"{newValue}";
+         else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string userText = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string newText = Replace(userText, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'K');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
